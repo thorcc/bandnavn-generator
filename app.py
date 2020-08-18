2c2280a12a8183561957a91b4fb530f9b10870bf
thorcc <thorchrcow@gmail.com>
2020-08-18 20:56:08 +0200

    la til teller

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 counter = 0
+
 adjektiv = []
 banneord = []
 with open('adjektiv.txt') as f:
@@ -11,14 +12,9 @@ with open('adjektiv.txt') as f:
 with open('banneord.txt') as f:
     banneord = f.read().splitlines()
 
-tilfeldig_adj = randint(0, len(adjektiv))
-tilfeldig_banne = randint(0, len(banneord))
-print(tilfeldig_adj, tilfeldig_banne)
-print(adjektiv[tilfeldig_adj] + " " + banneord[tilfeldig_banne])
-
-
 @app.route('/')
-def hello_world():
+def index():
+    global counter
     counter = counter + 1
     rnd_adj = randint(0, len(adjektiv))
     rnd_banne = randint(0, len(banneord))
